8b8a663f25c66e06e8f21cd60c62da70f295082f
Joao victor <victorespucoc@gmail.com>
2021-03-21 19:28:03 -0300

    conexao

--- principal.php
+++ principal.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
+<html lang="en">
 <?php
 require_once("Classe/conexao.php");
+echo "hello"
 ?>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
