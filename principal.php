0ac5f23efee585565266553ada17e6f83cbab9fb
Joao victor <victorespucoc@gmail.com>
2021-03-21 19:52:08 -0300

    conexao

--- principal.php
+++ principal.php
@@ -146,7 +146,7 @@
     
     <!-- Importando o js do bootstrap -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
+    <script src="./js/principal.js" type="text/javascript"></script>
 </body>
 
 </html>
