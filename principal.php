93be2d69d930a3dcaf32094dcdd3bd66144c59d8
Joao victor <victorespucoc@gmail.com>
2021-03-24 12:04:57 -0300

    gravar

--- principal.php
+++ principal.php
@@ -2,7 +2,7 @@
 	ini_set('display_errors',1);
 	ini_set('display_startup_erros',1);
 	error_reporting(E_ALL);
-	require_once("../Classes/Admissao.php");
+	require_once("Classes/Principal.php");
 
     $geral = new Geral();
     $listarCliente = $geral->listarOptionsCliente(NULL);
