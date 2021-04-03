8752cfb656f2a1f966da0d63f591452e9e1455c1
Joao victor <victorespucoc@gmail.com>
2021-04-03 10:43:29 -0300

    teste banco

--- principal.php
+++ principal.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!--<link href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css" rel="stylesheet"/>
     <script scr="https://code.jquery.com/jquery-3.5.1.js"></script>-->
-    <script scr="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+    
     <script src="js/sweetalert.js"></script>
 
 
@@ -153,6 +153,7 @@
         </div>       
     </div>
     <!-- Importando o js do bootstrap -->
+    <script scr="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
     <script src="./js/datatables.bundle.js" type="text/javascript"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="./js/principal.js" type="text/javascript"></script>
