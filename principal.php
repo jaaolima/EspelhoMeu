f501fdae6e61bf6f94b8072360cac37a0e576bfa
Joao victor <victorespucoc@gmail.com>
2021-04-03 11:01:47 -0300

    teste banco

--- principal.php
+++ principal.php
@@ -6,7 +6,7 @@
 
     $Principal = new Principal();
     $listarCliente = $Principal->listarOptionsCliente();
-    $listarPonto = $Principal->listarPonto($_POST);
+    $retorno = $Principal->listarPonto($_POST);
 ?>
 <!DOCTYPE html>
 <html lang="en">
