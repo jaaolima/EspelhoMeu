5e7db421d6a9cd1174664b8f4469bf8a8ca40928
Joao victor <victorespucoc@gmail.com>
2021-04-03 10:49:22 -0300

    teste banco

--- js/principal.js
+++ js/principal.js
@@ -139,64 +139,7 @@ function validarPonto()
 
 	return true;
 }
-var DatatablesBasicBasic = function() {
 
-	var initTable1 = function() {
-		var table = $('#table');
-
-		// begin first table
-		table.DataTable({
-			responsive: true,
-			retrieve: true,
-			buttons: [
-	            'copyHtml5', 'csvHtml5', 'excelHtml5', 'pdfHtml5', 'print'
-	        ],
-			
-
-			//== DOM Layout settings
-			dom: `Bf<'row'<'col-sm-12'tr>>
-			<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
-
-			lengthMenu: [5, 10, 25, 50],
-
-			pageLength: 10,
-
-			language: {
-				'lengthMenu': 'Mostrar _MENU_',
-			},
-			"oLanguage": {
-			    "sSearch": "<span>Pesquisar:</span> _INPUT_",
-			    "sLengthMenu": "<span>Mostrar:</span> _MENU_",
-			    "sInfo": "<span>Mostrando </span>_START_ até _END_ de _TOTAL_",
-			    "sZeroRecords": "Não existem dados cadastrados",
-			    "sInfoEmpty": "<span>Mostrando </span>0 até 0 de 0",
-			    "oPaginate": { "sFirst": "Primeira", "sLast": "Última", "sNext": ">", "sPrevious": "<" }
-		    },
-
-			//== Order settings
-			"order": [],
-
-
-		});
-	
-
-		table.on('change', 'tbody tr .m-checkbox', function() {
-			$(this).parents('tr').toggleClass('active');
-		});	
-		
-	};
-
-	return {
-
-		//main function to initiate the module
-		init: function() {
-			initTable1();
-		},
-
-	};
-
-}();
-
-jQuery(document).ready(function() {
-	DatatablesBasicBasic.init();
-});
+$(document).ready(function() {
+    $('#table').DataTable();
+} );
