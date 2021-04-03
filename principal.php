b65ebeed438fe431fb85c28ddfe1eb992d37a337
Joao victor <victorespucoc@gmail.com>
2021-04-03 10:48:02 -0300

    teste banco

--- principal.php
+++ principal.php
@@ -14,12 +14,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Universo</title>
-    <link href="./css/datatables.min.css" rel="stylesheet" type="text/css" />
+
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!--<link href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css" rel="stylesheet"/>
     <script scr="https://code.jquery.com/jquery-3.5.1.js"></script>-->
-    
     <script src="js/sweetalert.js"></script>
 
 
@@ -152,11 +151,18 @@
             </div>
         </div>       
     </div>
-    <!-- Importando o js do bootstrap -->
+    <!-- Importando o jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+    <!-- Importando o Datatable-->
+    <link href="./css/datatables.min.css" rel="stylesheet" type="text/css" />
     <script scr="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
     <script src="./js/datatables.bundle.js" type="text/javascript"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="./js/principal.js" type="text/javascript"></script>
+    
+    <!-- Importando o js do bootstrap -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+   
 </body>
 
 </html>
